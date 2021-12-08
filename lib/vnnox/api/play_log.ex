b7212f117938e24bed7c2ee09e9d263f4ffc1139
Vanilla Hsu <vanilla@fatpipi.com>
2021-12-08 15:43:06 +0800

    add get_playlogs_summary/3

--- lib/vnnox/api/play_log.ex
+++ lib/vnnox/api/play_log.ex
@@ -12,4 +12,14 @@ defmodule VNNOX.Api.PlayLog do
       start: start
     })
   end
+
+  def get_playlogs_summary(id, start_date, end_date, start \\ 0, count \\ 20) do
+    do_get("v1/playLog/getPlayLogsSummary", %{
+      playerId: id,
+      startTime: start_date,
+      endTime: end_date,
+      count: count,
+      start: start
+    })
+  end
 end
